accc05ddf1b0c566127242e92c24bbc940b84abb
kjisoo <kim@jisoo.net>
2019-11-27 11:18:57 +0900

    Support carthage

---
+++ Punycode-Cocoa/Punycode-Cocoa/Punycode_Cocoa.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Punycode_Cocoa.
+FOUNDATION_EXPORT double Punycode_CocoaVersionNumber;
+
+//! Project version string for Punycode_Cocoa.
+FOUNDATION_EXPORT const unsigned char Punycode_CocoaVersionString[];
+
+#import <Punycode_Cocoa/NSStringPunycodeAdditions.h>
+#import <Punycode_Cocoa/WebNSURLExtras.h>
